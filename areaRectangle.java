151dde804ce29cde9eb8125065b8f7dfc1877864
UTKARSH GUPTA <67434242+utkarshgupta22@users.noreply.github.com>
2021-10-02 12:41:37 +0530

    Add files via upload

---
+++ areaRectangle.java
@@ -0,0 +1,9 @@
+public class areaRectangle {
+    public static void main(String args[])  
+    {  
+    int width=2;  
+    int height=7;  
+    int area=width*height;  
+        System.out.println("Area of rectangle="+area);  
+     }  
+}
